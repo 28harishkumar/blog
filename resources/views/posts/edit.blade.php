30c873b82aae837d59e34d2d9461f2a279274d75
Harish Kumar <28harish.kumcr@gmail.com>
2015-06-29 09:41:17 +0530

    upload image to server option is added to tinyMCE editor

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -9,8 +9,8 @@ Edit Post
 <script type="text/javascript">
 	tinymce.init({
 		selector : "textarea",
-		plugins : ["advlist autolink lists link image charmap print preview anchor", "searchreplace visualblocks code fullscreen", "insertdatetime media table contextmenu paste"],
-		toolbar : "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image"
+		plugins : ["advlist autolink lists link image charmap print preview anchor", "searchreplace visualblocks code fullscreen", "insertdatetime media table contextmenu paste jbimages"],
+		toolbar : "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image jbimages"
 	}); 
 </script>
 
